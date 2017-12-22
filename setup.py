006c36e580fe6a9d17279ea9442cde81668e0ee8
Thang Duong <thang@quantee.com>
2017-12-22 11:57:26 +0000

    build a wheel

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+	def is_pure(self):
+		return False
+
+setup(name='tokenex',
+      version='0.1',
+      description='A flexible tokenizer',
+      url='http://aka.ms/tokenex',
+      author='Thang Duong',
+      author_email='thduon@microsoft.com',
+      license='Microsoft Internal Only',
+      packages=['tokenex'],
+      package_data={
+           'tokenex': ['tokenizer_so.so', 'tokenizer.dll'],
+      },
+      include_package_data=True,
+      distclass=BinaryDistribution,
+      zip_safe=False)
